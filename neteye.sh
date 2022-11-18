4b06728addca1742ca25bd4aec5048e618057a80
Letder40 <adrian04rv@gmail.com>
2022-11-18 01:47:27 +0000

    the eye is looking at you

--- neteye.sh
+++ neteye.sh
@@ -5,41 +5,86 @@
  end="\e[0m"
  green="\e[32m"
  blue="\e[34m"
+ NOT_24=false
 
 if [[ $1 == "--help" ]]; then
   echo "neteye -i {net ip address} -p {port}"
   exit 0
 fi
 
-if [[ $1 = "-i" ]]; then
-  if [[ $2 =~ ^[0-9]{1,3}\.[0-9]{1,3}\.[0-9]{1,3}\.[0-9]{1,3}$ ]]; then
-    
-    if [[ $3 = "-p" ]]; then
-      if [[ $4 =~ ^[0-9]{1,5}$  ]]; then   
-       net=$(echo $2 | cut -d "." -f1,2,3)
-       echo -e "\n${yellow}[!] only nets with /24 are valids${end}\n"   
-       echo -e "${red} The eye is open ${end}\n"
-       for i in $(seq 1 254) ; do
-        target="$net.$i"
-        timeout 1 bash -c "echo '' > /dev/tcp/$target/$4" 2> /dev/null && echo -e "${yellow}->${end} $target has $4 port ${green}open${end}" &
-       
-       done; wait
-       echo -e "\n${red} The eye is closed ${end}"
+if [[ $1 == "--internet" && $2 == "-p" && $3 =~ [0-9]{1,5} && $3 -le 65536 ]]; then   
+echo -e "\n${red} The eye is open ${end}\n"
+for host1 in $(seq 1 254); do
+  for host2 in $(seq 1 254); do
+    for host3 in $(seq 1 254); do
+      for host4 in $(seq 1 254); do
+        target="$host1.$host2.$host3.$host4"
+        echo -ne " ( 0) ${red}looking${end} at --> ${green}$target${end}\r"
+          timeout 1 bash -c "echo '' > /dev/tcp/$target/$2" 2> /dev/null && echo -e "${yellow}->${end} $target has $4 port ${green}open${end}                        " &
+        done
+      done
+    done
+  done; wait
+  echo -e "\n\n${red} The eye is closed ${end}"
+fi
 
-      else
-
-        echo "ports invalid"
-
-      fi
-
-    fi 
-
-    exit 0
-  else 
-    echo "ip address not valid"
-    exit 1 
+if [[ $# -ge 5 ]]; then
+if [[ $5 == "-m" ]]; then
+  if [[ $6 == 8 || $6 == 16 || $6 == 24 ]]; then
+    NOT_24=true
+  else
+    echo "only class A | B | C , ip address are valids"
   fi
 else
-  echo -e "\nsyntax: neteye -i {net ip address} -p {port}\n"
+  echo "invalid argument, try ( -m ) if you want to select a netmask"
+  exit 1
 fi
+fi
+
+if [[ $1 = "-i" && $2 =~ ^[0-9]{1,3}\.[0-9]{1,3}\.[0-9]{1,3}\.[0-9]{1,3}$ && $3 = "-p" && $4 =~ ^[0-9]{1,5}$ && $4 -le 65536 ]]; then
+  echo -e "\n${red} The eye is open ${end}\n"
+  
+  if [[ $NOT_24 == false || $6 == 24 ]]; then
+    net=$(echo $2 | cut -d "." -f1,2,3)
+    for host4 in $(seq 1 254) ; do
+      target="$net.$host4"
+      echo -ne " ( 0) ${red}looking${end} at --> ${green}$target${end}\r"
+      timeout 1 bash -c "echo '' > /dev/tcp/$target/$4" 2> /dev/null && echo -e "${yellow}->${end} $target has $4 port ${green}open${end}                           " &   
+    done; wait
+  fi
+
+  if [[ $NOT_24 == true ]]; then
+    
+    if [[ $6 == 8 ]]; then
+      net=$(echo $2 | cut -d "." -f1)
+      for host2 in $(seq 1 254); do
+        for host3 in $(seq 1 254); do
+          for host4 in $(seq 1 254); do
+            target="$net.$host2.$host3.$host4"
+            echo -ne " ( 0) ${red}looking${end} at --> ${green}$target${end}\r"
+            timeout 1 bash -c "echo '' > /dev/tcp/$target/$4" 2> /dev/null && echo -e "${yellow}->${end} $target has $4 port ${green}open${end}                     " &
+          done
+        done
+      done; wait
+    fi
+
+    if [[ $6 == 16 ]]; then
+      net=$(echo $2 | cut -d "." -f1,2)
+      for host3 in $(seq 1 254); do
+       for host4 in $(seq 1 254); do
+          target="$net.$host3.$host4"
+          echo -ne " ( 0) ${red}looking${end} at --> ${green}$target${end}\r"
+          timeout 1 bash -c "echo '' > /dev/tcp/$target/$4" 2> /dev/null && echo -e "${yellow}->${end} $target has $4 port ${green}open${end}                     " &
+        done
+      done; wait
+    fi
+
+  fi
+    echo -e "\n\n${red} The eye is closed ${end}"
+else  
+  echo -e "\nsyntax: neteye -i {net ip address} -p {port}\n"
+  exit 1
+fi
+
+
 
